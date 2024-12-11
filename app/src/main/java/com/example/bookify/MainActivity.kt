1238652b4a593be0c98773e1508e963db9d2d084
Silvi Dian Pramana <129763597+Vivin28@users.noreply.github.com>
2024-12-11 08:52:35 +0700

    perbaikan 3

--- app/src/main/java/com/example/bookify/MainActivity.kt
+++ app/src/main/java/com/example/bookify/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.bookify
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -11,7 +10,6 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
         // Handling window insets
